13d9f52dcf62d41ae665dc6b18c3bf5ec00e2b33
saif1642 <islumsaiful@gmail.com>
2022-01-24 18:16:22 +0600

    updated

---
+++ client/src/utils/constant.js
@@ -0,0 +1,9 @@
+import abi from './Transaction.json';
+
+
+//Contract Details
+
+
+export const contractAbi = abi.abi;
+export const contractAddress = `0x8d6b2f86e3F805DC00EcF770f9611B73EA38A617`;
+
